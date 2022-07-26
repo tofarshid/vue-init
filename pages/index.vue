4f8d896ac28207c2764d28a75b4ada328b8f7d7f
Faz <Farshid.Kamal@service.nsw.gov.au>
2022-07-27 08:09:20 +1000

    init

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <Test />
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Test from "~/components/Test.vue";
+
+export default Vue.extend({
+  name: "IndexPage",
+  components: { Test },
+});
+</script>
